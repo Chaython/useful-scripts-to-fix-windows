67b7aee4fccb9184ac175df390a6303a98725c8a
Chaython <chaython@live.ca>
2024-01-29 12:42:11 -0700

    Add files via upload
    A bunch of random scripts I've needed to use in the past

---
+++ Disable DNS.bat
@@ -0,0 +1,1 @@
+REG add "HKLM\SYSTEM\CurrentControlSet\services\Dnscache" /v Start /t REG_DWORD /d 4 /f
---
+++ disable LLMNR and network discovery.bat
@@ -0,0 +1,3 @@
+REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient”
+REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient” /v ” EnableMulticast” /t REG_DWORD /d “0” /f 
+netsh advfirewall firewall set rule group="Network Discovery" new enable=No
